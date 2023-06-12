64528f8725bc419cd55ac7b079089596eb34e333
nitesh1996-lara <95542470+nitesh1996-lara@users.noreply.github.com>
2023-06-12 15:01:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title('Nature is very good today')
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 and Bluberry Oatmeal')
+streamlit.text('Banana Shake and Oreo')
+streamlit.text('Boiled eggs and Sweets')
+streamlit.text('Veg Thali and Salad')
