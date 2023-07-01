2044a296bea7815eb7c233f0176dbbe02091d724
nitesh1996-lara <95542470+nitesh1996-lara@users.noreply.github.com>
2023-07-01 22:57:49 +0530

    Update streamlit_app.py
    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,12 +41,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 # except URLError as e:
 #    streamlit.error()
 
-streamlit.stop()
-#Snowflake-related functions
-def get_fruit_load_list():
-   with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from fruit_load_list")
-        return my_cur.fetchall()
+# streamlit.stop()
+# #Snowflake-related functions
+# def get_fruit_load_list():
+#    with my_cnx.cursor() as my_cur:
+#         my_cur.execute("select * from fruit_load_list")
+#         return my_cur.fetchall()
 
 
 # Add a button to load the fruit
