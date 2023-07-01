70d0ee11ca39b9c47d5a14d4a2952e60be186f98
nitesh1996-lara <95542470+nitesh1996-lara@users.noreply.github.com>
2023-07-01 22:14:22 +0530

    Update streamlit_app.py
    last commit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,12 +25,12 @@ try:
     if not fruit_choice:
        streamlit.write('The user entered ', fruit_choice)
     else:
-   #import requests
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-   # write your own comment -what does the next line do? 
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   # write your own comment - what does this do?
-   streamlit.dataframe(fruityvice_normalized)
+       #import requests
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+       # write your own comment -what does the next line do? 
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       # write your own comment - what does this do?
+       streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
    streamlit.error()
 
